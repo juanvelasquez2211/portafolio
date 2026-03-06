1a36759c4f1a8ab89d6809d37c6d05b447781c48
juanvelasquez2211 <juanvelasquez2211@gmail.com>
2026-03-06 11:59:11 -0500

    Add files via upload

---
+++ particulas.js
@@ -0,0 +1,13 @@
+tsParticles.load("particles", {
+particles: {
+number: { value: 60 },
+size: { value: 2 },
+move: { speed: 1 },
+color: { value: "#8b5cf6" },
+links: {
+enable: true,
+color: "#8b5cf6",
+distance: 150
+}
+}
+});
